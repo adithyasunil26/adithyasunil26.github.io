7b0d5dfafc248c243630893e104eddfb472832a4
adithyasunil26 <adithyasunil26@gmail.com>
2021-08-09 18:10:42 +0400

    Added projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,9 +20,8 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
    <li class="nav-item">
-   <input type="checkbox" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
+   <input type="checkbox" id ="checktog" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
    </li>
    <div class="bike">
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-80 0 650 400" preserveAspectRatio="xMinYMin meet">
@@ -243,11 +242,11 @@ $(document).ready(function () {
 if (localStorage.getItem("lightMode") == "dark") {
   var app = document.getElementsByTagName("HTML")[0];
   app.setAttribute("light-mode", "dark");
-
   //to add dark theme to nav bar after its been loaded
   window.addEventListener("load", function () {
     var nav = document.getElementById("navbar");
     nav.classList.add("dark-theme");
+    document.getElementById("checktog").checked = true;
   });
 
   var sc = document.getElementsByClassName("socialicon");
@@ -284,6 +283,7 @@ function toggle_light_mode() {
 window.addEventListener("storage", function () {
   if (localStorage.lightMode == "dark") {
     app.setAttribute("light-mode", "dark");
+    document.getElementsById("check").checked = true;
   } else {
     app.setAttribute("light-mode", "light");
   }
