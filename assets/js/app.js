135a66928173bdf913538706cec66b32737caeb9
adithyasunil26 <adithyasunil26@gmail.com>
2021-08-09 21:14:36 +0400

    Added blog href in navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://adithyasunil26.medium.com/">Blog</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/file/d/1JLVn9PD2vUwZjZaNcmDcZyq-icg0E31N/view?usp=sharing">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id ="checktog" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
